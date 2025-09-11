70b0cf6d3b9f6eea844fc85e3a0103952c16b504
krcolonia <krcolonia@gmail.com>
2025-09-12 00:40:10 +0800

    Updated app timezone to be Asia/Manila, removed isVerified column temporarily from User migration to prevent errors

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-			$table->boolean('isVerified');
+			// $table->boolean('isVerified');
             $table->rememberToken();
             $table->timestamps();
         });
